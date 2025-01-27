d8fa1225169b7b9cd4bdddb8b68a74c7c61f930e
dlgarciabr <dlgarciabr@gmail.com>
2025-01-27 22:42:47 +0000

    added graphic

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import React from "react";
-import { Container, Grid2 as Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Container, Grid2 as Grid, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import Decimal from 'decimal.js';
+import { PieChart } from "@mui/x-charts";
 
 const mockedData = [
   {
@@ -75,26 +76,34 @@ const Result = () => {
 
   const calcTotal = () => mockedData.reduce((prev, {groups}) => prev + calcGroupTotal(groups), 0);
 
-  const calcPercentual = (value: number) => formatNumber(value * 100 / calcTotal(), 1);
+  const calcPercentual = (value: number) => value * 100 / calcTotal();
+
+  const calcFormattedPercentual = (value: number) => formatNumber(calcPercentual(value), 1);
+
+  let index = 0;
 
   const renderRows = () => {
     const rows: TableRow[] = [];
 
     mockedData.forEach(data => {
-      rows.push({ id: 1, columnA: data.title, columnC: formatNumber(calcGroupTotal(data.groups), 2), columnD: `${calcPercentual(calcGroupTotal(data.groups))}%`});
+      rows.push({ id: index, columnA: data.title, columnC: formatNumber(calcGroupTotal(data.groups), 2), columnD: `${calcFormattedPercentual(calcGroupTotal(data.groups))}`});
+      index++;
       data.groups.forEach(group => {
         if(group.items.length === 1){
-          rows.push({ id: 1, columnA: group.title, columnB : group.items[0].title, columnC: formatNumber(group.items[0].value, 2), columnD: calcPercentual(group.items[0].value) });
+          rows.push({ id: index, columnA: group.title, columnB : group.items[0].title, columnC: formatNumber(group.items[0].value, 2), columnD: calcFormattedPercentual(group.items[0].value) });
         } else if(group.items.length > 1 ) {
-          rows.push({ id: 1, columnA: group.title, columnB : 'total movel', columnC: formatNumber(calcSubgroupTotal(group.items), 2), columnD: `${calcPercentual(calcSubgroupTotal(group.items))}%`, span: group.items.length + 1 })
-          group.items.forEach( item => { 
-            rows.push({ id: 1, columnA: undefined, columnB : item.title, columnC: formatNumber(item.value, 2), columnD: calcPercentual(item.value) })
+          rows.push({ id: index, columnA: group.title, columnB : 'total movel', columnC: formatNumber(calcSubgroupTotal(group.items), 2), columnD: `${calcFormattedPercentual(calcSubgroupTotal(group.items))}`, span: group.items.length + 1 });
+          index++;
+          group.items.forEach( item => {
+            rows.push({ id: index, columnA: undefined, columnB : item.title, columnC: formatNumber(item.value, 2), columnD: calcFormattedPercentual(item.value) });
+            index++;
           });
         }
+        index++;
       });
     });
-  
-    rows.push({ id: 1, columnA: 'Total', columnC: formatNumber(calcTotal(), 2), columnD: '100%' });
+
+    rows.push({ id: index, columnA: 'Total', columnC: formatNumber(calcTotal(), 2), columnD: '100%' });
 
     return rows.map(renderRow);
   }
@@ -102,7 +111,7 @@ const Result = () => {
   const renderRow = (row: TableRow) => {
     return (
       <TableRow
-        key={1}
+        key={row.id}
         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
       >
         {
@@ -128,13 +137,14 @@ const Result = () => {
 
   return (
     <Container maxWidth="md">
-      <Grid container spacing={2} style={{height: '700px'}}>
-        <Grid size={{ xs: 12, md: 12 }} container justifyContent='center' alignContent='center' style={{textAlign: 'center'}}>
+      <Grid container spacing={2} style={{height: '900px'}}>
+        {/* <Grid size={{ xs: 12, md: 12 }} container justifyContent='center' alignContent='center' style={{textAlign: 'center'}}>
           <Typography gutterBottom variant="h5" component="div">
             Resultado
           </Typography>
-        </Grid>
-        <Grid size={{ xs: 12, md: 12 }} container justifyContent='center' alignContent='center' style={{textAlign: 'center'}}>
+        </Grid> */}
+        {/* <Grid size={{ xs: 12, md: 12 }} container justifyContent='center' alignContent='center' style={{textAlign: 'center'}}> */}
+        <Stack direction="row" width="100%" textAlign="center" spacing={2}>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
@@ -150,7 +160,29 @@ const Result = () => {
               </TableBody>
             </Table>
           </TableContainer>
-        </Grid>
+        </Stack>
+        {/* </Grid> */}
+        <Stack direction="row" width="100%" textAlign="center" spacing={2}>
+          <Box flexGrow={1}>
+            <PieChart
+              series={[
+                {
+                  data: [
+                    { id: 0, value: calcPercentual(calcGroupTotal(mockedData[0].groups)), label: mockedData[0].title },
+                    { id: 1, value: calcPercentual(calcGroupTotal(mockedData[1].groups)), label: mockedData[1].title },
+                  ],
+                  innerRadius: 50,
+                  outerRadius: 130,
+                  paddingAngle: 5,
+                  cornerRadius: 5,
+                  startAngle: 0,
+                  // cx: 150,
+                  // cy: 150,
+                }
+              ]}
+            />
+          </Box>
+        </Stack>
       </Grid>
     </Container>
   );
