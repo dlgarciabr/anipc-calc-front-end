9b05f01b921b2ceb43051d114aaa56f7acc05d3c
dlgarciabr <dlgarciabr@gmail.com>
2025-01-27 19:00:30 +0000

    added table

--- src/app/calculator/api.ts
+++ src/app/calculator/api.ts
@@ -3,7 +3,7 @@ import { CalculatorResult, RequestForm, SimulationData } from "../../types";
 
 const mocks = {
     getForm: false,
-    calculate: false,
+    calculate: true,
 }
 
 const newMockdData = {
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,55 +1,131 @@
 "use client"
 
-import { Paper } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import React from "react";
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import dynamic from "next/dynamic";
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+const mockedData = [
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    title: 'Ambito 1',
+    groups: [
+      {
+        title: 'Combustão Estacionaria',
+        items: [{
+          title: 'Gás propano',
+          value: 7.54
+        }] 
+      },
+      {
+        title: 'Combustão Móvel',
+        items: [
+          {
+            title: 'Gasóleo',
+            value: 1289
+          },
+          {
+            title: 'Gasolina',
+            value: 38
+          }
+        ] 
+      }
+    ]
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+    title: 'Ambito 2',
+    groups: [{
+      title: 'Energia Elétrica',
+      items: [{
+        title: 'Energia Elétrica (faturada)',
+        value: 3616
+      }] 
+    }]
+  }
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+interface DatagridRow {
+  id: number; 
+  span?: number;
+  columnA: string | undefined;
+  columnB?: string; 
+  columnC?: number;
+  columnD?: string;
+}
 
-const paginationModel = { page: 0, pageSize: 5 };
+const Result = () => {
+  const calcTotal = () => {
+    // return childRows.reduce((prev, { columnC }) => prev + (columnC ? columnC : 0), 0);
+    return 999999;
+  }
 
-const Result = () => (
-  <Paper sx={{ height: 400, width: '100%' }}>
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      initialState={{ pagination: { paginationModel } }}
-      pageSizeOptions={[5, 10]}
-      checkboxSelection
-      sx={{ border: 0 }}
-    />
-</Paper>
-)
+  const renderRows = () => {
+    const rows: DatagridRow[] = [];
+
+    mockedData.forEach(data => {
+      rows.push({ id: 1, columnA: data.title, columnC: calcTotal()});
+      data.groups.forEach(group => {
+        if(group.items.length === 1){
+          rows.push({ id: 1, columnA: group.title, columnB : group.items[0].title, columnC: group.items[0].value, columnD: 'CALCULAR' });
+        } else if(group.items.length > 1 ) {
+          rows.push({ id: 1, columnA: group.title, columnB : group.items[0].title, columnC: group.items[0].value, columnD: '99%', span: group.items.length })
+          group.items.slice(1, group.items.length).forEach( item => { 
+            rows.push({ id: 1, columnA: undefined, columnB : item.title, columnC: item.value, columnD: '99%' })
+          });
+        }
+      });
+    });
+  
+    rows.push({ id: 1, columnA: 'Total', columnC: 4962, columnD: '100%calc', span: 1 });
+
+    return rows.map(renderRow);
+  }
+
+  const renderRow = (row: DatagridRow) => {
+    return (
+      <TableRow
+        key={1}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        {
+          row.columnA ?
+          (
+            <TableCell component="th" scope="row" rowSpan={row.span || 1}>
+              {row.columnA}
+            </TableCell>
+          ): undefined
+        }
+        <TableCell component="th" scope="row">
+          {row.columnB}
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          {row.columnC}
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+          {row.columnD}
+        </TableCell>
+      </TableRow>
+    );
+  }
+
+  return (
+    <Container maxWidth="md">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Fontes de emissão</TableCell>
+              <TableCell align="center">Fontes de energia</TableCell>
+              <TableCell align="center">Emissão de CO2</TableCell>
+              <TableCell align="center">%</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {renderRows()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+}
 
 export default dynamic(() => Promise.resolve(Result), {
   ssr: false
